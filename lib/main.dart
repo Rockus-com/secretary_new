733520dde9a57d881b2881e3ac4f91ef87ae34e4
Rockus <eduardtepanosyan@gmail.com>
2024-12-27 00:13:33 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,7 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: MyHomePage(),
     );
@@ -18,7 +20,9 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  MyHomePage({super.key});
+  final TextEditingController login = TextEditingController();
+  final TextEditingController password = TextEditingController();
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -30,9 +34,33 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title:const Text("secretary"),
+        title: const Text("secretary"),
       ),
-      body: const Center(),
+      body: Center(
+          child: Column(
+        children: [
+          TextField(
+            decoration: const InputDecoration(label: Text("Логин")),
+            controller: widget.login,
+          ),
+          TextField(
+            decoration: const InputDecoration(label: Text("Пароль")),
+            controller: widget.password,
+          ),
+          FloatingActionButton(
+              onPressed: () {
+                Login(widget.login.text, widget.password.text);
+              },
+              child: const Text("Войти"))
+        ],
+      )),
     );
   }
+}
+
+void Login(login, password) {
+  
+//   variable for connect socket
+  // print(login);
+  // print(password);
 }
